9b98326c3198d2b78b9e0e3ea04d09cecb94da5f
Super-Potato1 <lx2005185203@163.com>
2026-04-18 20:35:30 +0800

    新增登录页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/login/index.vue'
+import Home from '@/views/home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,11 @@ const router = createRouter({
       name: 'login',
       component: Login,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about'
-    //   route level code-splitting
-    //   this generates a separate chunk (About.[hash].js) for this route
-    //   which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+    },
   ],
 })
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import {useRouter} from 'vue-router'
+import { ElMessage } from 'element-plus'
+import type { FormInstance, FormRules } from "element-plus"
+interface RuleForm {
+  account: string
+  password: string
+}
+const router = useRouter();
+const form = reactive({
+  account: '',
+  password: '',
+})
+const ruleFormRef = ref<FormInstance>()
+const formRules = reactive<FormRules<RuleForm>>({
+  account: [
+    {
+      required: true,
+      message: '请输入账号',
+      trigger: 'blur',
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur',
+    },
+  ],
+})
+const onLogin = async (formRef: FormInstance | undefined) => {
+  if (!formRef) return
+  await formRef.validate((valid, fields) => {
+    if (valid) {
+      ElMessage({
+        message: '登录成功',
+        type: 'success',
+        plain: true,
+        placement: 'top',
+        offset: 80,
+        duration:1000
+      })
+      router.push('/home')
+    } else {
+      console.log(valid, fields)
+    }
+  })
+}
+</script>
 
 <template>
   <div class="login-background">
-    <div class="login-box"></div>
+    <div class="login-form">
+      <p>登录</p>
+      <el-form class="form-container" ref="ruleFormRef" :model="form" :rules="formRules" label-width="auto">
+        <el-form-item label="账号" prop="account">
+          <el-input v-model="form.account" placeholder="请输入账号" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password"">
+          <el-input v-model="form.password" placeholder="请输入密码" show-password />
+        </el-form-item>
+        <!-- 这里就是给登录/注册按钮预留的位置 -->
+        <div class="btn-group">
+          <el-button type="primary" @click="onLogin(ruleFormRef)">登录</el-button>
+          <el-button>注册</el-button>
+        </div>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -12,17 +76,81 @@
   height: 100%;
   background-color: black;
   background:
-    linear-gradient(
-      to right,
+    linear-gradient(to right,
       #050508 0%,
-      rgba(5,5,8) 10%,
-      rgba(7,7,9) 20%,
+      rgba(5, 5, 8) 10%,
+      rgba(7, 7, 9) 20%,
       transparent 28%,
       transparent 72%,
-      rgba(7,7,9) 80%,
-      rgba(5,5,8) 90%,
-      #050508 100%
-    ),
+      rgba(7, 7, 9) 80%,
+      rgba(5, 5, 8) 90%,
+      #050508 100%),
     url('../../assets/images/loginBackground.jpg') no-repeat center center / contain;
+  position: relative;
+}
+
+.login-form {
+  width: 700px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.3);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    line-height: 100px;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+    color: rgba(255, 255, 255, 0.7);
+    margin: 0;
+  }
+}
+
+.form-container {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 10px 0px;
+
+  .el-form-item {
+    margin-bottom: 0;
+
+    .el-input {
+      width: 300px;
+      height: 45px;
+    }
+  }
+
+  .btn-group {
+    margin-top: 20px;
+    display: flex;
+    gap: 15px;
+  }
+
+  :deep(.el-form-item__label-wrap) {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+  }
+
+  :deep(.el-form-item__label) {
+    color: rgba(255, 255, 255, 0.7);
+    font-size: 16px;
+    /* 文字大小 */
+    font-weight: 500;
+    /* 文字粗细 */
+    padding-right: 12px;
+    /* 离输入框的距离 */
+  }
 }
 </style>
